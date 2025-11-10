ce1402d6318df4b69f9a8dcab7de14e7591afea1
Arsalan Ahmed <162651120+arsalanahmed05@users.noreply.github.com>
2025-11-10 14:26:21 -0800

    changes

--- includes/header.php
+++ includes/header.php
@@ -1,57 +1,31 @@
 <header id="top">
     <div class="container-fluid">
-        <nav class="navbar bg-body-tertiary fixed-top">
-            <a class="navbar-brand" href="/"><img src="/assets/images/Feminize-digital.png" alt=""
-                    class="img-fluid"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <i class="fa-solid fa-bars"></i>
-            </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <img src="/assets/images/Feminize-digital.png" alt="" class="img-fluid dropdown-logo">
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div class="row">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="/assets/images/techifys-logo-web-development-agency.png" alt=""></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/home/">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/about-us/">About Us</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="/services/" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Services
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/content-writing/">Content Writing</a></li>
-                                <li><a class="dropdown-item" href="/email-marketing/">Email Marketing</a></li>
-                                <li><a class="dropdown-item" href="/graphic-designing/">Graphic Designing</a></li>
-                                <li><a class="dropdown-item" href="/search-engine/">SEO</a></li>
-                                <li><a class="dropdown-item" href="/social-media/">Social Media Marketing</a></li>
-                                <li><a class="dropdown-item" href="/web-designing/">Web Designing</a></li>
-                                <li><a class="dropdown-item" href="/web-development/">Web Development</a></li>
-                                <li><a class="dropdown-item" href="/pay-per-click/">PPC Marketing</a></li>
-
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/portfolio/">Portfolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/testimonials/">Testimonials</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/contact-us/">Contact Us</a>
-                        </li>
-                    </ul>
-                    <a href="/contact-us/"><button class="quote-btn bton"><i class="fa-brands fa-rocketchat"></i>get a quote</button></a>
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     </div>
 </header>
 
